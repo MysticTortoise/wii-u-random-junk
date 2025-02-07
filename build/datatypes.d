8067e171ab209d53509e10da239b7c043e76c0d5
MysticTortoise <58190981+MysticTortoise@users.noreply.github.com>
2025-02-06 23:52:43 -0800

    Delete build directory

--- build/datatypes.d
+++
@@ -1,4 +0,0 @@
-datatypes.o: \
- C:/Users/Denis/OneDrive/Documents/WIIU/GFX2/wii-u-random-junk/source/Engine/datatypes.cpp \
- C:/Users/Denis/OneDrive/Documents/WIIU/GFX2/wii-u-random-junk/include/datatypes.hpp
-C:/Users/Denis/OneDrive/Documents/WIIU/GFX2/wii-u-random-junk/include/datatypes.hpp:
